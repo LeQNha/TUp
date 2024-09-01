6bbe545c5e3724d2d6d462c42bc3fb70f2f6db17
LeQNha <leqnha684@gmail.com>
2024-09-01 16:38:03 +0700

    first commit

---
+++ app/src/main/java/nha/tu/tup/ui/acitvities/SignUp.kt
@@ -0,0 +1,12 @@
+package nha.tu.tup.ui.acitvities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nha.tu.tup.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
